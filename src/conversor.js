613a9feb252a892f76bd5ba3c58211cad5cc5224
Fabio <dahofcmmejia@gmail.com>
2023-03-21 03:36:09 -0400

    TDD Al introducir 2 deberia mostrar II

--- src/conversor.js
+++ src/conversor.js
@@ -1,5 +1,17 @@
-function conversor(numero){
-    return 'I';
+function conversor(numero) {
+    const valoresDecimales = [5, 4, 1];
+    const valoresRomanos = ["V", "IV", "I"];
+
+    let romano = "";
+
+    for (let i = 0; i < valoresDecimales.length; i++) {
+        while (numero >= valoresDecimales[i]) {
+            romano += valoresRomanos[i];
+            numero -= valoresDecimales[i];
+        }
+    }
+
+    return romano;
 }
 
 export default conversor;
--- src/conversor.spec.js
+++ src/conversor.spec.js
@@ -4,4 +4,7 @@ describe("Convertir decimal a romano", () =>{
     it("Al introducir 1 deberia mostrar I", () => {
         expect(conversor(1)).toEqual('I');
     });
+    it("Al introducir 2 deberia mostrar II", () => {
+        expect(conversor(2)).toEqual('II');
+    });
 });
