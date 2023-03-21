539178de129114976a29dcd907e97064078e20a1
Fabio <dahofcmmejia@gmail.com>
2023-03-21 03:59:58 -0400

    TDD Al introducir 5 deberia mostrar V

--- src/conversor.spec.js
+++ src/conversor.spec.js
@@ -7,6 +7,12 @@ describe("Convertir decimal a romano", () =>{
     it("Al introducir 2 deberia mostrar II", () => {
         expect(conversor(2)).toEqual('II');
     });
+    it("Al introducir 5 deberia mostrar V", () => {
+        expect(conversor(5)).toEqual('V');
+    });
+    it("Al introducir 7 deberia mostrar VII", () => {
+        expect(conversor(7)).toEqual('VII');
+    });
     it("Al introducir 77 deberia mostrar LXXVII", () => {
         expect(conversor(77)).toEqual('LXXVII');
     });
